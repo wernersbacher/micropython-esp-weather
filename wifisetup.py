f02702e6d12cc8242dbb0aacde39dc0a946c8507
Markus Wernersbach <wernersbacher@gmail.com>
2020-02-19 20:22:03 +0000

    wifisetup extended

--- wifisetup.py
+++ wifisetup.py
@@ -6,7 +6,11 @@ def setup():
 	ap_if = network.WLAN(network.AP_IF)
 	ap_if.active(False)
 	sta_if = network.WLAN(network.STA_IF)
-	sta_if.active(True)
-	sta_if.connect(config.SSID, config.PASS)
+	if not sta_if.isconnected():
+        	#print('Connecting to WiFi...')
+	        sta_if.active(True)
+	        sta_if.connect(config.SSID, config.PASS)
+       		while not sta_if.isconnected():
+	            time.sleep(1)
 
 	return sta_if.isconnected()
