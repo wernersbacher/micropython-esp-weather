0d761739c025996412563696fdbc2460f36a5177
wernersbacher <wernersbacher@gmail.com>
2020-02-20 18:47:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import web
 def run():
 	try:
 		wifisetup.setup()
-		temperature, humidity = hardware.get_temperature_and_humidity()
+		temperature, humidity = boardhandling.get_temperature_and_humidity()
 		print('Temperature = {temperature}, Humidity = {humidity}'.format(
 			temperature=temperature, humidity=humidity))
 		web.send_temperature(temperature, humidity)
 	except Exception as exc:
 		print(exc)
-		hardware.show_error()
+		boardhandling.show_error()
 
-	if not hardware.is_debug():
+	if not boardhandling.is_debug():
 		pass
 		#hardware.deepsleep()
 
